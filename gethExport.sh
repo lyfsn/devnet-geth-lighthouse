56f65851eb9e005a56afb947eed6d1e07777e6bb
wangyufsn <dev.wangyu@proton.me>
2023-11-23 10:56:47 +0800

    add export data shell

---
+++ gethExport.sh
@@ -0,0 +1,3 @@
+./geth export --state.scheme=path --datadir=./execution-data --syncmode=snap export.data.snap.tar.gz
+./geth export --state.scheme=path --datadir=./execution-data --syncmode=full export.data.full.tar.gz
+./geth export --state.scheme=path --datadir=./execution-data --syncmode=light export.data.light.tar.gz
