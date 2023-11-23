68bb272e95ba363b80392ae356c4488174dd4fb4
wangyufsn <dev.wangyu@proton.me>
2023-11-23 11:44:47 +0800

    up

--- startGeth.sh
+++ startGeth.sh
@@ -19,7 +19,8 @@
   --authrpc.addr=127.0.0.1 \
   --authrpc.vhosts=* \
   --authrpc.jwtsecret=./el-cl-genesis-data/jwt/jwtsecret \
-  --syncmode=archive \
+  --syncmode=snap \
+  --gcmode=archive \
   --rpc.allow-unprotected-txs \
   --metrics \
   --metrics.addr=0.0.0.0 \
